a297db5b6c629da9fbed2e2e79dd9d385b75ef25
Yetendra Bhandari <yetendrabhandari@gmail.com>
2021-03-22 00:56:14 +0530

    article 10 update

--- pages/article/10.vue
+++ pages/article/10.vue
@@ -79,8 +79,8 @@ class JavaInput {
       <p>
         What if we need to input numbers and not strings. The Scanner class has
         a number of methods for capturing primitive data-types. These work
-        similar to the <em>next()</em> method by returning the next group of
-        non-whitespace characters. The following are the most commonly used
+        similar to the <em>next()</em> method by returning the next token(group
+        of non-whitespace characters). The following are the most commonly used
         ones.
       </p>
       <ul class="mt-4 list-disc list-inside">
@@ -125,10 +125,37 @@ class JavaInput {
 }'
         lang="language-java"
       />
+      <div class="mt-6 flex flex-col">
+        <h3 class="text-xl">BufferedReader</h3>
+        <div class="w-4 h-1 bg-background-2 rounded-full"></div>
+      </div>
+      <p class="mt-4">
+        The Scanner class provides an easy-to-remember way of taking user input
+        but it has one major disadvantage. It performs poorly when there is a
+        very large amount of data. This becomes a determining factor in
+        competitive programming.<br />
+        BufferedReader, due to its larger buffer size, performs much better. It
+        is also synchronized, meaning that it is thread-safe, when working in a
+        multi-threading environment.<br /><br />
+        Let's look at a basic example of BufferedReader.
+      </p>
+      <CodeSnippet
+        code='import java.util.Scanner; // Import the Scanner class.
+class JavaInput {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.next(); // John
+        System.out.println("Hello " + name); // Hello John
+    }
+}'
+        lang="language-java"
+      />
       <p>
-        Scanner does not have a separate method to read in a character. Instead
-        we can use the <em>next()</em> method to input a single character string
-        and use <em>charAt(0)</em> to get the first and only character.
+        Let's start with an example of Scanner class. It is present in the
+        <em>java.util</em> package. To use it, we need to create an instance of
+        the Scanner class tied to <em>System.in</em>(input stream which is
+        typically connected to keyboard input of console programs).
       </p>
     </article>
   </main>
