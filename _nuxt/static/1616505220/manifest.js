2355ab5b133e81c10647c3f3178b034516cd589d
Yetendra Bhandari <yetendrabhandari@gmail.com>
2021-03-23 18:43:41 +0530

    Updates

---
+++ _nuxt/static/1616505220/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farticle\u002F1","\u002Ffind","\u002Farticle\u002F10","\u002Fabout-us","\u002Farticle\u002F2","\u002Farticle\u002F4","\u002Farticle\u002F3","\u002Farticle\u002F5","\u002Farticle\u002F6","\u002Farticle\u002F8","\u002Farticle\u002F7","\u002Farticle\u002F9"]})
