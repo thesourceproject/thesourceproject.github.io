d519a6f117f3d52a024c9fb8738f369718639a84
Yetendra Bhandari <yetendrabhandari@gmail.com>
2021-04-04 18:49:05 +0530

    Updates

--- _nuxt/static/1617385688/article/2-Publishing-your-first-project-to-Github/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617385688",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002F2-Publishing-your-first-project-to-Github",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
